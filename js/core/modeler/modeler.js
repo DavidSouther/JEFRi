e0ff1f8e730f8e1c3d6e6b0e685e0cd0b2c38f63
David Souther <davidsouther@gmail.com>
2012-04-08 16:47:36 -0400

    Basic load/save work.

--- js/core/modeler/modeler.js
+++ js/core/modeler/modeler.js
@@ -36,12 +36,12 @@ $.widget("ui.entity", {
 	_header_clicked: function(e) {
 		var text = $(e.target).text();
 		var input =
- 			$("<input type='text' />")
+ 			$("<input type='text' class='name' />")
 				.val(text)
 				.blur($.proxy(function(e){
-					var text = $(e.target).val();
+					text = $(e.target).val() || text;
 					$(e.target).parents('h3').html(
-						$("<span />").text(text)
+						$("<span class='name' />").text(text)
 							.click($.proxy(this._header_clicked, this))
 					);
 				}, this));
@@ -75,6 +75,42 @@ $.widget("ui.entity", {
 		new_li.find('a').aclick(this._expand_field, this).text(field);
 		new_li.find('.type').text(type);
 		li.replaceWith(new_li);
+	},
+
+	name: function(name) {
+		if(name === undefined) {
+			return this.element.find(".name").text();
+		} else {
+			this.element.find(".name").text(name);
+			return this;
+		}
+	},
+
+	addField: function(name, type, attributes){
+		var new_field = $("#templates .new_field").clone();
+		new_field.find('.name').text(name);
+		new_field.find('.type').text(type);
+		this.element.find('.field-list > li:last').before(new_field);
+	},
+
+	save: function(){
+		var entity = {
+			name: this.element.find('.name:first').text(),
+			key: "",
+			properties: [],
+			relationships: [],
+			attributes: {}
+		};
+
+		this.element.find('.field-list > li:not(:last)').each(function(){
+			var field = {
+				name: $(this).find('.name').text(),
+				type: $(this).find('.type').text(),
+				attributes: {}
+			};
+			entity.properties.push(field);
+		});
+		return entity;
 	}
 });
 
@@ -86,6 +122,42 @@ $("#new_entity").click(function(){
 
 $("#load_context").click(function(){
 	$("#loadSave").dialog("open");
+	$("#loadSave_do").val("Load").click(function(){
+		loadContext($("#loadSave > textarea").val());
+		$("#loadSave").dialog("close");
+	})
 });
+
+$("#save_context").click(function(){
+	$("#loadSave").dialog("open");
+	$("#loadSave > textarea").val(saveContext());
+	$("#loadSave_do").val("Done").click(function(){
+		$("#loadSave").dialog("close");
+	})
+});
+
+var loadContext = function(text){
+	var rawContext = window.JSON.parse(text);
+	$.each(rawContext.entities, function(){
+		var entity = $("#templates .entity").clone().entity()
+		.entity("name", this.name);
+		$.each(this.properties, function(){
+			entity.entity('addField', this.name, this.type);
+		});
+		entity.appendTo($("#entities"));
+	});
+	console.log(rawContext);
+};
+
+var saveContext = function(){
+	var context = {
+		meta: {},
+		entities: []
+	}
+	$("#entities .entity").each(function(){
+		context.entities.push($(this).entity('save'));
+	});
+	return window.JSON.stringify(context);
+};
 
 });
