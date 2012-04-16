366eabaa19bfd9542c92f49eba0b020735745c29
David Souther <davidsouther@gmail.com>
2012-04-16 18:18:21 -0400

    JSHint fixes.

--- js/core/uuid.js
+++ js/core/uuid.js
@@ -1,7 +1,7 @@
 /**
  * Renamespaced to be UUID class by David Souther, 2011
  */
-;//TODO move this to a separate file, and make it a class that can handle itself
+//TODO move this to a separate file, and make it a class that can handle itself
 //as either strings or (preferably) ints.
 
 // Build several namespaces, globally...
@@ -24,13 +24,13 @@ UUID.v5 = function(msg, namespace) {
 	nst = bin(namespace || '00000000-0000-0000-0000-000000000000');
 
 	var hash = Sha1.hash(nst + msg, true);
-	var uuid =  hash.substring(0, 8)	//8 digits
-		+ '-' + hash.substring(8, 12)	//4 digits
+	var uuid =  hash.substring(0, 8) +	//8 digits
+		'-' + hash.substring(8, 12)	+ //4 digits
 //			// four most significant bits holds version number 5
-		+ '-' + ((parseInt(hash.substring(12, 16), 16) & 0x0fff) | 0x5000).toString(16)
+		'-' + ((parseInt(hash.substring(12, 16), 16) & 0x0fff) | 0x5000).toString(16) +
 //			// two most significant bits holds zero and one for variant DCE1.1
-		+ '-' + ((parseInt(hash.substring(16, 20), 16) & 0x3fff) | 0x8000).toString(16)
-		+ '-' + hash.substring(20, 32)	//12 digits
+		'-' + ((parseInt(hash.substring(16, 20), 16) & 0x3fff) | 0x8000).toString(16) +
+		'-' + hash.substring(20, 32);	//12 digits
 	return uuid;
 };
 
@@ -58,7 +58,7 @@ var bin = function(uuid) {
 	}
 
 	return bin;
-}
+};
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 /*  SHA-1 implementation in JavaScript | (c) Chris Veness 2002-2010 | www.movable-type.co.uk      */
@@ -84,7 +84,7 @@ Sha1.hash = function(msg, utf8encode) {
 	// constants [§4.2.1]
 	var K = [0x5a827999, 0x6ed9eba1, 0x8f1bbcdc, 0xca62c1d6];
 
-	// PREPROCESSING 
+	// PREPROCESSING
 
 	msg += String.fromCharCode(0x80);  // add trailing '1' bit (+ 0's padding) to string [§5.1.1]
 
@@ -96,14 +96,14 @@ Sha1.hash = function(msg, utf8encode) {
 	for (var i=0; i<N; i++) {
 		M[i] = new Array(16);
 		for (var j=0; j<16; j++) {  // encode 4 chars per integer, big-endian encoding
-			M[i][j] = (msg.charCodeAt(i*64+j*4)<<24) | (msg.charCodeAt(i*64+j*4+1)<<16) | 
+			M[i][j] = (msg.charCodeAt(i*64+j*4)<<24) | (msg.charCodeAt(i*64+j*4+1)<<16) |
 				(msg.charCodeAt(i*64+j*4+2)<<8) | (msg.charCodeAt(i*64+j*4+3));
 		} // note running off the end of msg is ok 'cos bitwise ops on NaN return 0
 	}
 	// add length (in bits) into final pair of 32-bit integers (big-endian) [§5.1.1]
 	// note: most significant word would be (len-1)*8 >>> 32, but since JS converts
 	// bitwise-op args to 32 bits, we need to simulate this by arithmetic operators
-	M[N-1][14] = ((msg.length-1)*8) / Math.pow(2, 32); M[N-1][14] = Math.floor(M[N-1][14])
+	M[N-1][14] = ((msg.length-1)*8) / Math.pow(2, 32); M[N-1][14] = Math.floor(M[N-1][14]);
 	M[N-1][15] = ((msg.length-1)*8) & 0xffffffff;
 
 	// set initial hash value [§5.3.1]
@@ -138,15 +138,15 @@ Sha1.hash = function(msg, utf8encode) {
 
 		// 4 - compute the new intermediate hash value
 		H0 = (H0+a) & 0xffffffff;  // note 'addition modulo 2^32'
-		H1 = (H1+b) & 0xffffffff; 
-		H2 = (H2+c) & 0xffffffff; 
-		H3 = (H3+d) & 0xffffffff; 
+		H1 = (H1+b) & 0xffffffff;
+		H2 = (H2+c) & 0xffffffff;
+		H3 = (H3+d) & 0xffffffff;
 		H4 = (H4+e) & 0xffffffff;
 	}
 
-	return Sha1.toHexStr(H0) + Sha1.toHexStr(H1) + 
+	return Sha1.toHexStr(H0) + Sha1.toHexStr(H1) +
 		Sha1.toHexStr(H2) + Sha1.toHexStr(H3) + Sha1.toHexStr(H4);
-}
+};
 
 /**
  * function 'f' [§4.1.1]
@@ -158,25 +158,25 @@ Sha1.f = function(s, x, y, z)  {
 	case 2: return (x & y) ^ (x & z) ^ (y & z);  // Maj()
 	case 3: return x ^ y ^ z;                    // Parity()
 	}
-}
+};
 
 /**
  * rotate left (circular left shift) value x by n positions [§3.2.5]
  */
 Sha1.ROTL = function(x, n) {
 	return (x<<n) | (x>>>(32-n));
-}
+};
 
 /**
- * hexadecimal representation of a number 
- *   (note toString(16) is implementation-dependant, and  
+ * hexadecimal representation of a number
+ *   (note toString(16) is implementation-dependant, and
  *   in IE returns signed numbers when used on full words)
  */
 Sha1.toHexStr = function(n) {
 	var s="", v;
 	for (var i=7; i>=0; i--) { v = (n>>>(i*4)) & 0xf; s += v.toString(16); }
 	return s;
-}
+};
 
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
@@ -187,7 +187,7 @@ Sha1.toHexStr = function(n) {
 //var Utf8 = {};  // Utf8 namespace
 
 /**
- * Encode multi-byte Unicode string into utf-8 multiple single-byte characters 
+ * Encode multi-byte Unicode string into utf-8 multiple single-byte characters
  * (BMP / basic multilingual plane only)
  *
  * Chars in range U+0080 - U+07FF are encoded in 2 chars, U+0800 - U+FFFF in 3 chars
@@ -196,22 +196,22 @@ Sha1.toHexStr = function(n) {
  * @returns {String} encoded string
  */
 Utf8.encode = function(strUni) {
-	// use regular expressions & String.replace callback function for better efficiency 
+	// use regular expressions & String.replace callback function for better efficiency
 	// than procedural approaches
 	var strUtf = strUni.replace(
 			/[\u0080-\u07ff]/g,  // U+0080 - U+07FF => 2 bytes 110yyyyy, 10zzzzzz
-			function(c) { 
+			function(c) {
 				var cc = c.charCodeAt(0);
 				return String.fromCharCode(0xc0 | cc>>6, 0x80 | cc&0x3f); }
 		);
 	strUtf = strUtf.replace(
 			/[\u0800-\uffff]/g,  // U+0800 - U+FFFF => 3 bytes 1110xxxx, 10yyyyyy, 10zzzzzz
-			function(c) { 
-				var cc = c.charCodeAt(0); 
+			function(c) {
+				var cc = c.charCodeAt(0);
 				return String.fromCharCode(0xe0 | cc>>12, 0x80 | cc>>6&0x3F, 0x80 | cc&0x3f); }
 		);
 	return strUtf;
-}
+};
 
 /**
  * Decode utf-8 encoded string back into multi-byte Unicode characters
@@ -224,7 +224,7 @@ Utf8.decode = function(strUtf) {
 	var strUni = strUtf.replace(
 			/[\u00e0-\u00ef][\u0080-\u00bf][\u0080-\u00bf]/g,  // 3-byte chars
 			function(c) {  // (note parentheses for precence)
-				var cc = ((c.charCodeAt(0)&0x0f)<<12) | ((c.charCodeAt(1)&0x3f)<<6) | ( c.charCodeAt(2)&0x3f); 
+				var cc = ((c.charCodeAt(0)&0x0f)<<12) | ((c.charCodeAt(1)&0x3f)<<6) | ( c.charCodeAt(2)&0x3f);
 				return String.fromCharCode(cc); }
 		);
 	strUni = strUni.replace(
