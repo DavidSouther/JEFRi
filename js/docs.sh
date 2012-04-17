08ba8f8744d68eea7ba256b9914209f97554177a
David Souther <davidsouther@gmail.com>
2012-04-16 20:10:23 -0400

    Added script to build docs.

---
+++ js/docs.sh
@@ -0,0 +1,13 @@
+#\!/bin/sh 
+
+find . -name docs -exec rm -rf '{}' +
+mkdir docs
+find . -type d -not -path '*test*' -not -name . | while read d 
+	do cd $d 
+	docco *js 
+	cd - 
+	mkdir docs/$d/ 
+	mv $d/docs/* docs/$d/ 
+done
+
+mv docs ../docs/js
