a283422ce02ee06562f3562881327cf4ddabdee1
David Souther <davidsouther@gmail.com>
2012-04-21 18:00:00 -0400

    Cleaned and fixed dir return

--- docs/build.sh
+++ docs/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+rm -rf docs
+
 for project in $(find . -maxdepth 1 -mindepth 1 -type d -not -name docs -not -name latex )
 do
 	cd $project
@@ -7,7 +9,8 @@ do
 	mkdir _out_
 	make -f ../latex/Makefile
 	make -f ../latex/Makefile clean
-	cd ..
+	cd - 
 done
 
+mkdir docs
 find . -type f -path '*/_out_/*' | xargs -I{} cp {} ./docs/
