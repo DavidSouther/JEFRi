5272d8f515e6a7537c624287de9c113db245230a
David Souther <davidsouther@gmail.com>
2012-05-28 17:59:07 -0400

    Run docco on coffee as well.

--- js/docs.sh
+++ js/docs.sh
@@ -1,14 +1,14 @@
-#!/bin/sh 
+#!/bin/sh
 
 find . -name docs -type d -exec rm -rf '{}' +
 mkdir docs
 rm -rf ../docs/docs/js
-find . -type d -not -path '*test*' -not -name . | sed 's/\.\///' | while read d 
-	do cd $d 
-	$(which node) $(which docco) *js 
-	cd - 
-	mkdir docs/$d/ 
-	mv $d/docs/* docs/$d/ 
+find . -type d -not -path '*test*' -not -name . | sed 's/\.\///' | while read d
+	do cd $d
+	$(which node) $(which docco) *js *coffee
+	cd -
+	mkdir docs/$d/
+	mv $d/docs/* docs/$d/
 done
 
 mv docs ../docs/docs/js
