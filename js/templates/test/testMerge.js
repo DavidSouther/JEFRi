841cfd3c17d16efe067eef92188222fced307b35
David Souther <davidsouther@gmail.com>
2012-09-28 19:46:19 +0000

    Added test to kill comments in merge.

--- js/templates/test/testMerge.js
+++ js/templates/test/testMerge.js
@@ -13,13 +13,15 @@ test("Unit Testing Environment", function () {
 });
 
 test("Merge", function (){
-	var base, q = "<div><span class='d'></span></div>", a = "<div id='a'><div id='b'></div></div>", b = "<div id='a'><div id='c'></div></div><span class='e'></span>";
+	var base, q = "<div><span class='d'></span></div>", a = "<div id='a'><div id='b'></div></div>", b = "<div id='a'><div id='c'><span><!--comment--></span></div></div><span class='e'></span>", c = "<!--Comment!-->";
 	base = $(q);
 	ok($.fn.merge, "jQuery has no DOM Merge plugin.");
 	ok(base.merge($(a)).children("div").length === 1, "Appended first merge attempt.");
 	ok(base.merge($(b)).children("div").length === 1, "Merged second merge attempt.");
 	ok(base.children("#a").children().length === 2, "Appended merged children.");
 	ok(base.children().length === 2, "Replaced default elements?");
+	base.merge($(c));
+	equal(base.html().match(/<!--/), null, "No comments in merged code.");
 });
 
 }(jQuery));
