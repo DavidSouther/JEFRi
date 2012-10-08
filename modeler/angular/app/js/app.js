cfa03336fb25c4c2f4ca1dffec832e1ff1eeed7b
David Souther <davidsouther@gmail.com>
2012-10-08 16:28:35 -0400

    No need for routes.

--- modeler/angular/app/js/app.js
+++ modeler/angular/app/js/app.js
@@ -1,19 +1,8 @@
 (function(){
-  var routes, jefri;
+  var jefri;
   angular.module('modeler', ['jefri', 'jquery']);
-  routes = function($rp){
-    $rp.when('/Context', {
-      templateUrl: 'partials/context.html',
-      controller: 'Context'
-    }).when('/Entity', {
-      templateUrl: 'partials/entity.html',
-      controller: 'Entity'
-    }).otherwise({
-      redirectTo: '/Entity'
-    });
-  };
   jefri = function(JEFRi){
     JEFRi.load('entityContext.json');
   };
-  angular.module('modeler').config(['$routeProvider', routes]).run(['JEFRi', jefri]);
+  angular.module('modeler').run(['JEFRi', jefri]);
 }).call(this);
