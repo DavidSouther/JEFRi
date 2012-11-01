1547f9ce7b004d240631540bed27ea8e65371fbf
David Souther <davidsouther@gmail.com>
2012-11-01 13:49:04 -0400

    Updated superscore to 0.3.0rc1

--- js/core/test/qunit/assets/superscore.js
+++ js/core/test/qunit/assets/superscore.js
@@ -1,4 +1,7 @@
-;(function(){var underscore = this._;
+;(function(){
+var underscore = this._;
+
+
 //     superscore pubsub.js 0.2.3
 //     (c) 2012 David Souther
 //     superscore is freely distributable under the MIT license.
@@ -317,196 +320,6 @@ underscore.extend(underscore, {
 });
 
 }.call(this, underscore));
-
-//     superscore core.js 0.2.5
-//     (c) 2012 David Souther
-//     superscore is freely distributable under the MIT license.
-//     For all details and documentation:
-//     https://github.com/DavidSouther/superscore
-
-(function(underscore, $){
-// Missing from Underscore.
-underscore.mixin({
-	// ### indexBy*(list, func)*
-	// The default underscore indexOf uses a literal value; we often want to use an comparator. This function returns the index of the first element in the list that the comparator returns truthy when evaluating, or -1 if no elements match.
-	indexBy: function(list, func) {
-		list = list || []; func = func || function(){return false;};
-		for (var i = 0, l = list.length; i < l; i++) {
-			if (func(list[i])){ return i; }
-		}
-		return -1;
-	},
-
-	// ### noop
-	noop: function(){},
-
-	// ### symmetricDifference*(set1, set2[, ...setN])*
-	// The symmetric of two sets is is the set of elements in either set, but not their intersection.
-	// If two sets are equal, the symmetric difference is empty.
-	symmetricDifference: function(){
-		return underscore.reduce(arguments, function(first, second){
-			return underscore.union(
-				underscore.difference(first, second),
-				underscore.difference(second, first)
-			);
-		});
-	},
-
-	// ### deep*(object, path[, value[, overwrite]])*
-	// Follow a path deep into an object, creating intermediate objects or arrays as necessary.
-	// If value is specified, sets the value of that key if unset. If overwrite is true, sets
-	// even if the value is already set. Returns the root object on set, or the current value
-	// on get.
-	deep: function(object, path, value, overwrite){
-		overwrite = overwrite || false;
-		value = value || null;
-
-		// Break the path, if it's not already an array.
-		path = underscore.isString(path) ? path.split('.') : underscore.isArray(path) ? path : [];
-		// Get the next step
-		var part = path.shift();
-		// Different behavior depending on if we're at the last step
-		if(path.length) {
-			// More children, so make sure there's a container at the next level
-			if(!object[part]){
-				object[part] = !underscore.isNaN(+path[0]) ? [] : {};
-			}
-			// Recurse, returning either the object or the old value
-			var next = underscore.deep(object[part], path, value, overwrite);
-			return value ? object : next;
-		} else {
-			// If no value, return the part.
-			if(!value){
-				return object[part];
-			} else {
-				object[part] = overwrite ? value : (object[part] || value);
-				return object;
-			}
-		}
-	}
-});
-
-// ## Underscore Utilities
-underscore._extend = underscore.extend;
-var hasOwn = Object.prototype.hasOwnProperty;
-
-// ### Underscore's extend doesn't do deep extension. Use jQuery's (^c/^v from jQuery core).
-underscore.extend = function() {
-	var options, name, src, copy, copyIsArray, clone,
-		target = arguments[0] || {},
-		i = 1,
-		length = arguments.length,
-		deep = false;
-
-	// Handle a deep copy situation
-	if ( typeof target === "boolean" ) {
-		deep = target;
-		target = arguments[1] || {};
-		// skip the boolean and the target
-		i = 2;
-	}
-
-	// Handle case when target is a string or something (possible in deep copy)
-	if ( typeof target !== "object" && !underscore.isFunction(target) ) {
-		target = {};
-	}
-
-	// extend jQuery itself if only one argument is passed
-	if ( length === i ) {
-		target = this;
-		--i;
-	}
-
-	for ( ; i < length; i++ ) {
-		// Only deal with non-null/undefined values
-		if ( (options = arguments[ i ]) !== null ) {
-			// Extend the base object
-			for ( name in options ) {
-				src = target[ name ];
-				copy = options[ name ];
-
-				// Prevent never-ending loop
-				if ( target === copy ) {
-					continue;
-				}
-
-				// Recurse if we're merging plain objects or arrays
-				if ( deep && copy && ( underscore.isPlainObject(copy) || (copyIsArray = underscore.isArray(copy)) ) ) {
-					if ( copyIsArray ) {
-						copyIsArray = false;
-						clone = src && underscore.isArray(src) ? src : [];
-
-					} else {
-						clone = src && underscore.isPlainObject(src) ? src : {};
-					}
-
-					// Never move original objects, clone them
-					target[ name ] = underscore.extend( deep, clone, copy );
-
-				// Don't bring in undefined values
-				} else if ( copy !== undefined ) {
-					target[ name ] = copy;
-				}
-			}
-		}
-	}
-	// Return the modified object
-	return target;
-};
-underscore.isPlainObject = function( obj ) {
-	// Must be an Object.
-	// Because of IE, we also have to check the presence of the constructor property.
-	// Make sure that DOM nodes and window objects don't pass through, as well
-	if ( !obj || !underscore.isObject(obj) || obj.nodeType || underscore.isWindow( obj ) ) {
-		return false;
-	}
-
-	try {
-		// Not own constructor property must be Object
-		if ( obj.constructor &&
-			!hasOwn.call(obj, "constructor") &&
-			!hasOwn.call(obj.constructor.prototype, "isPrototypeOf") ) {
-			return false;
-		}
-	} catch ( e ) {
-		// IE8,9 Will throw exceptions on certain host objects #9897
-		return false;
-	}
-
-	// Own properties are enumerated firstly, so to speed up,
-	// if last one is own, then all properties are own.
-
-	var key;
-	for ( key in obj ) {}
-
-	return key === undefined || hasOwn.call( obj, key );
-};
-
-underscore.isWindow = $ ? $.isWindow : function( obj ) {
-	return obj !== null && obj === obj.window;
-};
-
-underscore.lock = function( fn ) {
-	return function(){
-		var ret, ex;
-		if(!fn.__locked){
-			fn.__locked = true;
-			try {
-			ret = fn.apply(this, arguments);
-			} catch (e){
-				ex = e;
-			}
-		}
-		fn.__locked = false;
-		if(ex){
-			throw ex;
-		} else {
-			return ret;
-		}
-	};
-};
-
-}).call(this, underscore, (typeof $ !== 'undefined' && $));
 
 //     superscore deferred.js 0.2.0
 //     (c) 2012 David Souther
@@ -856,4 +669,376 @@ underscore.lock = function( fn ) {
 	};
 
 }.call(this, underscore));
+(function(_){
+  var browserRequest, serverRequest, request;
+  browserRequest = function(){
+    var jQuery, ajax, get, post;
+    jQuery = typeof jQuery === 'undefined' ? null : jQuery;
+    ajax = jQuery
+      ? jQuery.ajax
+      : function(options){
+        var d, XHR, xhr;
+        d = _.Deferred();
+        XHR = window.ActiveXObject || XMLHttpRequest;
+        xhr = new XHR('Microsoft.XMLHTTP');
+        xhr.open(options.type || (options.data ? 'POST' : 'GET'), options.url, true);
+        if (in$('overrideMimeType', xhr)) {
+          xhr.overrideMimeType(options.dataType || 'text/plain');
+        }
+        xhr.onreadystatechange = function(){
+          var _ref;
+          if (deepEq$(xhr.readyState, 4, '===')) {
+            if (deepEq$(_ref = xhr.status, 0, '===') || deepEq$(_ref, 200, '===')) {
+              d.resolve(xhr.responseText);
+            } else {
+              d.reject(new Error("Could not load " + options.url));
+            }
+            return;
+          }
+          return d.notify(xhr);
+        };
+        if (options.data) {
+          xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+          xhr.setRequestHeader("Content-length", options.data.length);
+          xhr.setRequestHeader("Connection", "close");
+        }
+        xhr.send(options.data || null);
+        return d.promise();
+      };
+    get = jQuery
+      ? jQuery.get
+      : function(url, options){
+        options = options || {};
+        options.url = url;
+        options.type = 'GET';
+        options.data = null;
+        return ajax(options);
+      };
+    post = jQuery
+      ? jQuery.post
+      : function(url, options){
+        options = options || {};
+        options.url = url;
+        options.type = 'POST';
+        return ajax(options);
+      };
+    return {
+      get: get,
+      post: post
+    };
+  };
+  serverRequest = function(){
+    var request, get, post;
+    request = require('request');
+    get = function(uri){
+      var d;
+      d = _.Deferred();
+      request.get(uri, function(err, success, body){
+        return d.resolve(body);
+      });
+      return d.promise();
+    };
+    post = function(uri, data){
+      var d;
+      d = _.Deferred();
+      return d.promise();
+    };
+    return {
+      get: get,
+      post: post
+    };
+  };
+  if (typeof window !== 'undefined') {
+    request = browserRequest();
+  } else {
+    request = serverRequest();
+  }
+  _.request = function(uri){
+    var d;
+    d = _.Deferred();
+    request.get(uri).then(function(body){
+      return d.resolve(body);
+    });
+    return d.promise();
+  };
+}.call(this, underscore));
+function in$(x, arr){
+  var i = 0, l = arr.length >>> 0;
+  while (i < l) if (x === arr[i++]) return true;
+  return false;
+}
+function deepEq$(x, y, type){
+  var toString = {}.toString, hasOwnProperty = {}.hasOwnProperty,
+      has = function (obj, key) { return hasOwnProperty.call(obj, key); };
+  first = true;
+  return eq(x, y, []);
+  function eq(a, b, stack) {
+    var className, length, size, result, alength, blength, r, key, ref, sizeB;
+    if (a == null || b == null) { return a === b; }
+    if (a.__placeholder__ || b.__placeholder__) { return true; }
+    if (a === b) { return a !== 0 || 1 / a == 1 / b; }
+    className = toString.call(a);
+    if (toString.call(b) != className) { return false; }
+    switch (className) {
+      case '[object String]': return a == String(b);
+      case '[object Number]':
+        return a != +a ? b != +b : (a == 0 ? 1 / a == 1 / b : a == +b);
+      case '[object Date]':
+      case '[object Boolean]':
+        return +a == +b;
+      case '[object RegExp]':
+        return a.source == b.source &&
+               a.global == b.global &&
+               a.multiline == b.multiline &&
+               a.ignoreCase == b.ignoreCase;
+    }
+    if (typeof a != 'object' || typeof b != 'object') { return false; }
+    length = stack.length;
+    while (length--) { if (stack[length] == a) { return true; } }
+    stack.push(a);
+    size = 0;
+    result = true;
+    if (className == '[object Array]') {
+      alength = a.length;
+      blength = b.length;
+      if (first) { 
+        switch (type) {
+        case '===': result = alength === blength; break;
+        case '<==': result = alength <= blength; break;
+        case '<<=': result = alength < blength; break;
+        }
+        size = alength;
+        first = false;
+      } else {
+        result = alength === blength;
+        size = alength;
+      }
+      if (result) {
+        while (size--) {
+          if (!(result = size in a == size in b && eq(a[size], b[size], stack))){ break; }
+        }
+      }
+    } else {
+      if ('constructor' in a != 'constructor' in b || a.constructor != b.constructor) {
+        return false;
+      }
+      for (key in a) {
+        if (has(a, key)) {
+          size++;
+          if (!(result = has(b, key) && eq(a[key], b[key], stack))) { break; }
+        }
+      }
+      if (result) {
+        sizeB = 0;
+        for (key in b) {
+          if (has(b, key)) { ++sizeB; }
+        }
+        if (first) {
+          if (type === '<<=') {
+            result = size < sizeB;
+          } else if (type === '<==') {
+            result = size <= sizeB
+          } else {
+            result = size === sizeB;
+          }
+        } else {
+          first = false;
+          result = size === sizeB;
+        }
+      }
+    }
+    stack.pop();
+    return result;
+  }
+}
+//     superscore core.js 0.2.5
+//     (c) 2012 David Souther
+//     superscore is freely distributable under the MIT license.
+//     For all details and documentation:
+//     https://github.com/DavidSouther/superscore
+
+(function(underscore, $){
+// Missing from Underscore.
+underscore.mixin({
+	// ### indexBy*(list, func)*
+	// The default underscore indexOf uses a literal value; we often want to use an comparator. This function returns the index of the first element in the list that the comparator returns truthy when evaluating, or -1 if no elements match.
+	indexBy: function(list, func) {
+		list = list || []; func = func || function(){return false;};
+		for (var i = 0, l = list.length; i < l; i++) {
+			if (func(list[i])){ return i; }
+		}
+		return -1;
+	},
+
+	// ### noop
+	noop: function(){},
+
+	// ### symmetricDifference*(set1, set2[, ...setN])*
+	// The symmetric of two sets is is the set of elements in either set, but not their intersection.
+	// If two sets are equal, the symmetric difference is empty.
+	symmetricDifference: function(){
+		return underscore.reduce(arguments, function(first, second){
+			return underscore.union(
+				underscore.difference(first, second),
+				underscore.difference(second, first)
+			);
+		});
+	},
+
+	// ### deep*(object, path[, value[, overwrite]])*
+	// Follow a path deep into an object, creating intermediate objects or arrays as necessary.
+	// If value is specified, sets the value of that key if unset. If overwrite is true, sets
+	// even if the value is already set. Returns the root object on set, or the current value
+	// on get.
+	deep: function(object, path, value, overwrite){
+		overwrite = overwrite || false;
+		value = value || null;
+
+		// Break the path, if it's not already an array.
+		path = underscore.isString(path) ? path.split('.') : underscore.isArray(path) ? path : [];
+		// Get the next step
+		var part = path.shift();
+		// Different behavior depending on if we're at the last step
+		if(path.length) {
+			// More children, so make sure there's a container at the next level
+			if(!object[part]){
+				object[part] = !underscore.isNaN(+path[0]) ? [] : {};
+			}
+			// Recurse, returning either the object or the old value
+			var next = underscore.deep(object[part], path, value, overwrite);
+			return value ? object : next;
+		} else {
+			// If no value, return the part.
+			if(!value){
+				return object[part];
+			} else {
+				object[part] = overwrite ? value : (object[part] || value);
+				return object;
+			}
+		}
+	}
+});
+
+// ## Underscore Utilities
+underscore._extend = underscore.extend;
+var hasOwn = Object.prototype.hasOwnProperty;
+
+// ### Underscore's extend doesn't do deep extension. Use jQuery's (^c/^v from jQuery core).
+underscore.extend = function() {
+	var options, name, src, copy, copyIsArray, clone,
+		target = arguments[0] || {},
+		i = 1,
+		length = arguments.length,
+		deep = false;
+
+	// Handle a deep copy situation
+	if ( typeof target === "boolean" ) {
+		deep = target;
+		target = arguments[1] || {};
+		// skip the boolean and the target
+		i = 2;
+	}
+
+	// Handle case when target is a string or something (possible in deep copy)
+	if ( typeof target !== "object" && !underscore.isFunction(target) ) {
+		target = {};
+	}
+
+	// extend jQuery itself if only one argument is passed
+	if ( length === i ) {
+		target = this;
+		--i;
+	}
+
+	for ( ; i < length; i++ ) {
+		// Only deal with non-null/undefined values
+		if ( (options = arguments[ i ]) !== null ) {
+			// Extend the base object
+			for ( name in options ) {
+				src = target[ name ];
+				copy = options[ name ];
+
+				// Prevent never-ending loop
+				if ( target === copy ) {
+					continue;
+				}
+
+				// Recurse if we're merging plain objects or arrays
+				if ( deep && copy && ( underscore.isPlainObject(copy) || (copyIsArray = underscore.isArray(copy)) ) ) {
+					if ( copyIsArray ) {
+						copyIsArray = false;
+						clone = src && underscore.isArray(src) ? src : [];
+
+					} else {
+						clone = src && underscore.isPlainObject(src) ? src : {};
+					}
+
+					// Never move original objects, clone them
+					target[ name ] = underscore.extend( deep, clone, copy );
+
+				// Don't bring in undefined values
+				} else if ( copy !== undefined ) {
+					target[ name ] = copy;
+				}
+			}
+		}
+	}
+	// Return the modified object
+	return target;
+};
+underscore.isPlainObject = function( obj ) {
+	// Must be an Object.
+	// Because of IE, we also have to check the presence of the constructor property.
+	// Make sure that DOM nodes and window objects don't pass through, as well
+	if ( !obj || !underscore.isObject(obj) || obj.nodeType || underscore.isWindow( obj ) ) {
+		return false;
+	}
+
+	try {
+		// Not own constructor property must be Object
+		if ( obj.constructor &&
+			!hasOwn.call(obj, "constructor") &&
+			!hasOwn.call(obj.constructor.prototype, "isPrototypeOf") ) {
+			return false;
+		}
+	} catch ( e ) {
+		// IE8,9 Will throw exceptions on certain host objects #9897
+		return false;
+	}
+
+	// Own properties are enumerated firstly, so to speed up,
+	// if last one is own, then all properties are own.
+
+	var key;
+	for ( key in obj ) {}
+
+	return key === undefined || hasOwn.call( obj, key );
+};
+
+underscore.isWindow = $ ? $.isWindow : function( obj ) {
+	return obj !== null && obj === obj.window;
+};
+
+underscore.lock = function( fn ) {
+	return function(){
+		var ret, ex;
+		if(!fn.__locked){
+			fn.__locked = true;
+			try {
+			ret = fn.apply(this, arguments);
+			} catch (e){
+				ex = e;
+			}
+		}
+		fn.__locked = false;
+		if(ex){
+			throw ex;
+		} else {
+			return ret;
+		}
+	};
+};
+
+}).call(this, underscore, (typeof $ !== 'undefined' && $));
+
 }).call(this);
